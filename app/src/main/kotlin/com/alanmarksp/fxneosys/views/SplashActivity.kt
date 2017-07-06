32f4cb601e993bc4eaa1060c971aca84b8186488
Alan Mark <alanmarksp@gmail.com>
2017-07-06 10:35:11 +0200

    Initial commit

---
+++ app/src/main/kotlin/com/alanmarksp/fxneosys/views/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.alanmarksp.fxneosys.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.alanmarksp.fxneosys.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
